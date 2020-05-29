eaa5057f71cf347d735fb548d925966aa81ce941
Georgi Korchakov <georgikorchakov@gmail.com>
2020-05-29 20:50:29 +0300

    [UPDATE] Everithing works with stack (only).

--- Compiler/compiler.py
+++ Compiler/compiler.py
@@ -15,33 +15,53 @@ class Variable:
 class Compiler:
     def __init__(self, statements):
         self.statements = statements
-        self.current_address = 0x00
-        self.variables = {}
         self.bytecode = []
+
+        self.current_address = 0x00
+        self.variables = [{}]
+        self.stack_top = 0
 
     def compile(self):
         for statement in self.statements:
             print(statement)
             statement.accept(self)
         
+        self.bytecode.append(EOF)
         return self.bytecode
 
-    def add_variable(self, data_type, name):
-        self.variables[name] = Variable(data_type, name, self.current_address)
-        self.current_address += self.variables[name].get_length()
+    def add_variable(self, data_type, name): # DONE
+        self.variables[-1][name] = Variable(data_type, name, self.stack_top)
+        self.stack_top += 1
+        #self.stack_top += self.variables[name].get_length()
 
     def get_var_address(self, name):
-        return self.variables[name].address
+        for scope_num in range(len(self.variables)):
+            scope = self.variables[-(scope_num + 1)]
+
+            if name in scope:
+                return self.stack_top - scope[name].address
+
+        raise Exception(f"Variable '{name}' not found!")
 
     def visit_expression_statement(self, statement):
         statement.expression.accept(self)
 
     def visit_declaration_statement(self, statement):
+        self.bytecode.append(DECL)
         self.add_variable(statement.data_type.data_type.lexeme, statement.identifier.lexeme)
 
-    def visit_block_statement(self, statement):
+    def visit_block_statement(self, statement): # DONE
+        if statement.create_scope == True:
+            self.variables.append({})
+        
         for stmt in statement.statements:
             stmt.accept(self)
+        
+        if statement.create_scope == True:
+            for _ in self.variables.pop():
+                self.bytecode.append(POP)
+                self.stack_top -= 1
+            
 
     def visit_if_statement(self, statement):
         def hex_print(arr):
@@ -110,6 +130,7 @@ class Compiler:
         self.bytecode.append(CJP)
         self.bytecode.append(0x00)
         self.bytecode.append(0x00)
+        self.stack_top -= 1
     
     def append_empty_jump(self):
         self.bytecode.append(JMP)
@@ -131,6 +152,7 @@ class Compiler:
     def visit_assignment_expression(self, expression):
         expression.expression.accept(self)
         self.bytecode.append(POP_ABSOLUTE)
+        self.stack_top -= 1
 
         address = self.get_var_address(expression.variable.variable.lexeme)
         self.bytecode.append(address & 0x00FF)
@@ -161,6 +183,8 @@ class Compiler:
         elif expression.operator.type == "less_equal":
             self.bytecode.append(LEQ)
         
+        self.stack_top -= 1
+        
     def visit_type_expression(self, expression):
         pass
 
@@ -172,6 +196,7 @@ class Compiler:
 
     def visit_literal_expression(self, expression):
         self.bytecode.append(PUSH_IMMEDIATE)
+        self.stack_top += 1
 
         if expression.literal > 255:
             error("UNKNOWN", "Literal exceedes maximum value!")
@@ -182,6 +207,7 @@ class Compiler:
     def visit_variable_expression(self, expression):
         self.bytecode.append(PUSH_ABSOLUTE)
         address = self.get_var_address(expression.variable.lexeme)
+        self.stack_top += 1
         self.bytecode.append(address & 0x00FF)
         self.bytecode.append((address & 0xFF00) >> 8)
 
@@ -203,10 +229,10 @@ class Compiler:
         def get_command_number(i):
             return to_eight_bit_hex(i & 0x00FF) + " " + to_eight_bit_hex((i & 0xFF00) >> 8) + ":  "
 
-        string = "------\nVariables\n"
-        for var_name in self.variables:
-            string += str(var_name) + " = " + str(to_eight_bit_hex(self.variables[var_name].address & 0x00FF)) + " " + str(to_eight_bit_hex((self.variables[var_name].address & 0xFF00) >> 8)) + "\n"
-        string += "------\n"
+        #string = "------\nVariables\n"
+        #for var_name in self.variables:
+        #    string += str(var_name) + " = " + str(to_eight_bit_hex(self.variables[var_name].address & 0x00FF)) + " " + str(to_eight_bit_hex((self.variables[var_name].address & 0xFF00) >> 8)) + "\n"
+        string = "------\n"
 
         i = 0
         while i != len(self.bytecode):
@@ -227,6 +253,8 @@ class Compiler:
             elif opcode == CJP:
                 string += get_command_number(i) + "CJP " + "PC(" + to_eight_bit_hex(self.bytecode[i + 1]) + " " + to_eight_bit_hex(self.bytecode[i + 2]) + ")\n"
                 i = i + 2
+            elif opcode == DECL:
+                string += get_command_number(i) + "DECL\n"
             elif opcode == ADD:
                 string += get_command_number(i) + "ADD\n"
             elif opcode == SUB:
@@ -247,7 +275,9 @@ class Compiler:
                 string += get_command_number(i) + "GEQ\n"
             elif opcode == LEQ:
                 string += get_command_number(i) + "LEQ\n"
-
+            elif opcode == EOF:
+                string += get_command_number(i) + "EOF\n"
+                
             i = i + 1
 
         return string
