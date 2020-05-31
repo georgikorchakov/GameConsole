118b30b627b485c7c53804e2edfafcd28bfe42c2
Georgi Korchakov <georgikorchakov@gmail.com>
2020-05-31 20:45:27 +0300

    [UPDATE] Added function interpretation to Interpreter.

--- Interpreter/Interpreter.ino
+++ Interpreter/Interpreter.ino
@@ -1,5 +1,5 @@
 #define STACK_LENGTH   512
-#define PROGRAM_SIZE   0x4f
+#define PROGRAM_SIZE   0x46
 
 // Macros
 #define _FETCH_16BIT_POINTER(instruction) (*((uint16_t*)(instruction + 1)))
@@ -33,6 +33,10 @@
 #define POP 0x12
 #define DECL 0x13
 
+#define JST 0x14
+#define RET 0x15
+#define STR 0x16
+
 //#define DEBUG
 #define MEASURE_TIME
 
@@ -41,12 +45,16 @@ typedef struct stack_t {
   uint8_t top;
 } stack_t;
 
+uint8_t rv_register = 0; // Return Value Register
+
 stack_t value_stack;
 uint16_t pc = 0;
+uint16_t address = 0;
 uint8_t popped_value;
 
+
 uint8_t program[PROGRAM_SIZE] = {
-  19, 1, 6, 4, 1, 0, 19, 1, 1, 4, 1, 0, 19, 1, 0, 4, 1, 0, 19, 1, 0, 4, 1, 0, 19, 1, 1, 4, 1, 0, 2, 1, 0, 2, 6, 0, 13, 17, 43, 0, 16, 77, 0, 2, 2, 0, 4, 3, 0, 2, 4, 0, 4, 2, 0, 2, 3, 0, 2, 3, 0, 6, 4, 4, 0, 2, 1, 0, 1, 1, 6, 4, 1, 0, 16, 30, 0, 18, 0
+ 16, 55, 0, 2, 1, 0, 1, 1, 15, 17, 15, 0, 16, 23, 0, 1, 1, 22, 18, 20, 16, 23, 0, 1, 36, 1, 0, 2, 3, 0, 1, 1, 7, 16, 3, 0, 21, 1, 50, 1, 0, 2, 4, 0, 1, 2, 7, 16, 3, 0, 21, 6, 22, 18, 20, 19, 1, 65, 1, 0, 1, 12, 16, 3, 0, 21, 4, 1, 0, 0
 };
 
 unsigned long long int start_time = 0;
@@ -65,6 +73,9 @@ void loop() {
   uint8_t * instruction = program + pc;
   
   #ifdef DEBUG
+    Serial.print("                                    PC: ");
+    Serial.print(pc);
+    Serial.print(" Instruction: ");
     Serial.println(*instruction, HEX);
   #endif
   
@@ -102,6 +113,7 @@ void loop() {
       #endif
       popped_value = pop(&value_stack);
       value_stack.stack[value_stack.top - _FETCH_16BIT_POINTER(instruction)] = popped_value;
+      //value_stack.stack[value_stack.top - _FETCH_16BIT_POINTER(instruction)] = pop(&value_stack);
       pc += 3;
       break;
     case POP_RELATIVE:
@@ -111,7 +123,7 @@ void loop() {
       break;
     case ADD:
       #ifdef DEBUG
-        Serial.print("Add (at ");
+        Serial.print("ADD (at ");
         Serial.print(value_stack.top, HEX);
         Serial.print(") ");
         Serial.print(value_stack.stack[value_stack.top - 1], HEX);
@@ -122,11 +134,20 @@ void loop() {
       pc += 1;
       break;
     case SUB:
+      #ifdef DEBUG
+        Serial.print("SUB (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" - ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       sub(&value_stack);
       pc += 1;
+      break;
     case MUL:
       #ifdef DEBUG
-        Serial.print("Multiply (at ");
+        Serial.print("MUL (at ");
         Serial.print(value_stack.top, HEX);
         Serial.print(") ");
         Serial.print(value_stack.stack[value_stack.top - 1], HEX);
@@ -137,12 +158,20 @@ void loop() {
       pc += 1;
       break;
     case DIV:
+      #ifdef DEBUG
+        Serial.print("DIV (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" / ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       divide(&value_stack);
       pc += 1;
       break;
     case EQU:
       #ifdef DEBUG
-        Serial.print("Compare (at ");
+        Serial.print("EQU (at ");
         Serial.print(value_stack.top, HEX);
         Serial.print(") ");
         Serial.print(value_stack.stack[value_stack.top - 1], HEX);
@@ -153,16 +182,32 @@ void loop() {
       pc += 1;
       break;
     case NEQ:
+      #ifdef DEBUG
+        Serial.print("NEQ (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" != ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       not_equal(&value_stack);
       pc += 1;
       break;
     case GRT:
+      #ifdef DEBUG
+        Serial.print("GRT (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" > ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       greater(&value_stack);
       pc += 1;
       break;
     case LES:
       #ifdef DEBUG
-        Serial.print("Compare (at ");
+        Serial.print("LES (at ");
         Serial.print(value_stack.top, HEX);
         Serial.print(") ");
         Serial.print(value_stack.stack[value_stack.top - 1], HEX);
@@ -173,30 +218,46 @@ void loop() {
       pc += 1;
       break;
     case GEQ:
+      #ifdef DEBUG
+        Serial.print("GEQ (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" >= ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       greater_equal(&value_stack);
       pc += 1;
       break;
     case LEQ:
+      #ifdef DEBUG
+        Serial.print("LEQ (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") ");
+        Serial.print(value_stack.stack[value_stack.top - 1], HEX);
+        Serial.print(" <= ");
+        Serial.println(value_stack.stack[value_stack.top - 2], HEX);
+      #endif
       less_equal(&value_stack);
       pc += 1;
       break;
     case JMP:
+      #ifdef DEBUG
+        Serial.print("JMP (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.print(") to ");
+        Serial.println(_FETCH_16BIT_POINTER(instruction), HEX);
+      #endif
       pc = _FETCH_16BIT_POINTER(instruction);
       break;
     case POP:
+      #ifdef DEBUG
+        Serial.print("POP (at ");
+        Serial.print(value_stack.top, HEX);
+        Serial.println(")");
+      #endif
       value_stack.top--;
       pc += 1;
-      break;
-    case CJP:
-      if(pop(&value_stack)){
-        #ifdef DEBUG
-          Serial.print("CJP to ");
-          Serial.println(_FETCH_16BIT_POINTER(instruction), HEX);
-        #endif
-        pc = _FETCH_16BIT_POINTER(instruction);
-      } else {
-        pc += 3;
-      }
       break;
     case DECL:
       #ifdef DEBUG
@@ -207,11 +268,46 @@ void loop() {
       value_stack.top++;
       pc += 1;
       break;
+    case RET:
+      #ifdef DEBUG
+        Serial.print("RET: ");
+        Serial.println(rv_register, HEX);
+      #endif
+      push(&value_stack, rv_register);
+      pc += 1;
+      break;
+    case STR:
+      rv_register = pop(&value_stack);
+      #ifdef DEBUG
+        Serial.print("STR: ");
+        Serial.println(rv_register, HEX);
+      #endif
+      pc += 1;
+      break;
+    case JST:
+      address = pop_16bit(&value_stack);
+      #ifdef DEBUG
+        Serial.print("JST to ");
+        Serial.println(address, HEX);
+      #endif
+      pc = address;
+      break;
+    case CJP:
+      #ifdef DEBUG
+        Serial.print("CJP to ");
+        Serial.println(_FETCH_16BIT_POINTER(instruction), HEX);
+      #endif
+      if(pop(&value_stack)){
+        pc = _FETCH_16BIT_POINTER(instruction);
+      } else {
+        pc += 3;
+      }
+      break;
     case EOF:
       #ifdef MEASURE_TIME
         end_time = micros();
         Serial.print("Program execution time = ");
-        Serial.print((int)(end_time - start_time));
+        Serial.print((long)(end_time - start_time));
         Serial.println("us");
       #endif
       print_stack();
@@ -222,12 +318,21 @@ void loop() {
 // Helpers
 void push(stack_t * stack, uint8_t number) {
   stack->stack[stack->top] = number;
-  stack->top++;
+  ++stack->top;
 }
 
 uint8_t pop(stack_t * stack){
-  stack->top--;
+  --stack->top;
   return stack->stack[stack->top];
+}
+
+uint16_t pop_16bit(stack_t * stack){
+  stack->top -= 2;
+  #ifdef DEBUG
+    Serial.print("pop_16bit: ");
+    Serial.println((stack->stack[stack->top] + (stack->stack[stack->top + 1] << 8)), HEX);
+  #endif
+  return stack->stack[stack->top] + (stack->stack[stack->top + 1] << 8);
 }
 
 void init_stack(stack_t * stack) {
@@ -235,52 +340,52 @@ void init_stack(stack_t * stack) {
 }
 
 static inline void add(stack_t * stack) {
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] += stack->stack[stack->top];
 }
 
 static inline void mul(stack_t * stack) {
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] *= stack->stack[stack->top];
 }
 
 static inline void sub(stack_t * stack) {
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] -= stack->stack[stack->top];
 }
 
 static inline void divide(stack_t * stack) {
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] /= stack->stack[stack->top];
 }
 
 static inline void equal(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] == stack->stack[stack->top]);
 }
 
 static inline void not_equal(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] != stack->stack[stack->top]);
 }
 
 static inline void greater(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] > stack->stack[stack->top]);
 }
 
 static inline void less(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] < stack->stack[stack->top]);
 }
 
 static inline void greater_equal(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] >= stack->stack[stack->top]);
 }
 
 static inline void less_equal(stack_t * stack){
-  stack->top--;
+  --stack->top;
   stack->stack[stack->top-1] = (stack->stack[stack->top-1] <= stack->stack[stack->top]);
 }
 
